894eeb1cbb003c22231f0046ff8d0bd5fe928cfa
PedrohSantos <pedroh.santos170@gmail.com>
2020-04-28 00:40:26 -0300

    Initial commit

---
+++ R/ex02.R
@@ -0,0 +1,13 @@
+#'Colar dois Argumentos
+#'
+#'Recebe um argumento x e outro y e retorna eles colados separados por um espaco
+#'
+#'@param x primeiro argumento
+#'@param y segundo argumento
+#'
+#'@return os dois argumentos colados separados por espaço
+#'
+#'@export
+colar <- function(x,y){
+  paste0(x,y)
+}
